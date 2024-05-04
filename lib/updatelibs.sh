24cf44ae03326d1e9f236dd7a09a417f5a91f949
Audrey Castillo <valax321@gmail.com>
2024-05-05 09:12:43 +1000

    Fix FNA3D not being built

--- lib/updatelibs.sh
+++ lib/updatelibs.sh
@@ -9,28 +9,7 @@
 # Clone repos if needed
 if [ ! -d "./SDL2/" ]; then
 	echo "SDL2 folder not found. Cloning now..."
-	git clone --branch SDL2 https://github.com/libsdl-org/SDL.git SDL2
-
-	echo ""
-fi
-
-if [ ! -d "./FNA3D/" ]; then
-	echo "FNA3D folder not found. Cloning now..."
-	git clone https://github.com/FNA-XNA/FNA3D.git --recursive
-
-	echo ""
-fi
-
-if [ ! -d "./FAudio/" ]; then
-	echo "FAudio folder not found. Cloning now..."
-	git clone https://github.com/FNA-XNA/FAudio.git
-
-	echo ""
-fi
-
-if [ ! -d "./Theorafile/" ]; then
-	echo "Theorafile folder not found. Cloning now..."
-	git clone https://github.com/FNA-XNA/Theorafile
+	git clone --depth 1 --branch release-2.30.3 https://github.com/libsdl-org/SDL.git SDL2
 
 	echo ""
 fi
@@ -49,31 +28,3 @@ if [ ! -d "./Vulkan-Loader" ]; then
 	echo "Vulkan-Loader folder not found. Cloning now..."
 	git clone https://github.com/KhronosGroup/Vulkan-Loader
 fi
-
-# Check for updates...
-#echo "Updating SDL2..."
-#cd SDL2 && git pull && cd ..
-
-#echo ""
-#echo "Updating FNA3D..."
-#cd FNA3D && git pull && git submodule update && cd ..
-
-#echo ""
-#echo "Updating FAudio..."
-#cd FAudio && git pull && cd ..
-
-#echo ""
-#echo "Updating Theorafile..."
-#cd Theorafile && git pull && cd ..
-
-#echo ""
-#echo "Updating MoltenVK..."
-#cd MoltenVK && git pull && git submodule update && cd ..
-
-#echo ""
-#echo "Updating Vulkan-Headers..."
-#cd Vulkan-Headers && git pull && cd ..
-
-#echo ""
-#echo "Updating Vulkan-Loader..."
-#cd Vulkan-Loader && git pull && cd ..
